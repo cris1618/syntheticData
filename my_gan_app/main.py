deba3570962068627815709d1f20acf74fe7feb3
Cristian Del Gobbo <cristiandelgobbo87@gmail.com>
2025-02-22 06:06:18 +0100

    Origin on Railway

--- my_gan_app/main.py
+++ my_gan_app/main.py
@@ -20,7 +20,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000"
-    "https://your-vercel-frontend-url.vercel.app" 
+    "https://syntheticdata-production.up.railway.app" 
 ]
 
 app.add_middleware(
