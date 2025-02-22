5fd1ee38689c55cd3b973291b5d901aa6f942e61
Cristian Del Gobbo <cristiandelgobbo87@gmail.com>
2025-02-22 03:32:45 +0100

    Added setup.sh

--- my_gan_app/setup.sh
+++ my_gan_app/setup.sh
@@ -4,5 +4,10 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
+# Debugging: Print Rust and Cargo versions
+echo "Checking Rust version..."
+rustc --version
+cargo --version
+
 # Install dependencies
 pip install --no-cache-dir -r requirements.txt
